9b98ece53ccde1a17db094b749219be6f147aa59
edward <edwardlfish@gmail.com>
2025-06-20 11:23:54 +0300

    book.controller.js complete

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,8 +1,19 @@
 import express from "express";
-import {addBook} from "../controller/book.controller.js";
+import {
+    addBook,
+    findBookByIsbn,
+    findBooksByAuthor, findBooksByPublisher,
+    removeBook,
+    updateBookTitle
+} from "../controller/book.controller.js";
 
 const router = express.Router();
 
 router.post("/book", addBook);
+router.get("/book/:isbn", findBookByIsbn);
+router.delete("/book/:isbn", removeBook);
+router.patch("/book/:isbn/title/:title", updateBookTitle);
+router.get("/books/author/:author", findBooksByAuthor);
+router.get("/books/publisher/:publisher", findBooksByPublisher);
 
 export default router;
