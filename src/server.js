1b4006d1ea7b832811cde0596d8689dc162b7799
edward <edwardlfish@gmail.com>
2025-06-20 12:05:05 +0300

    Author and Publisher functionality

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import express from "express";
 import {dbConnection} from "./config/database.js";
 import {syncModels} from "./model/index.js";
 import bookRoutes from "./routes/book.routes.js";
+import authorRoutes from "./routes/author.routes.js";
+import publisherRoutes from "./routes/publisher.routes.js";
 
 dotenv.config()
 
@@ -14,6 +16,8 @@ app.use(express.json())
 dbConnection()
 syncModels()
 app.use(bookRoutes)
+app.use(authorRoutes)
+app.use(publisherRoutes)
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
