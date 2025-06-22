8ce1beec11b678d413294b0b35039270e7c268bd
edward <edwardlfish@gmail.com>
2025-06-22 11:33:54 +0300

    findPublishersByAuthor with sequelize.query and SQL

--- src/controller/publisher.controller.js
+++ src/controller/publisher.controller.js
@@ -1,4 +1,5 @@
 import {Author, Book} from "../model/index.js";
+import {sequelize} from "../config/database.js";
 
 export const findPublishersByAuthor = async (req, res) => {
     try {
@@ -7,16 +8,17 @@ export const findPublishersByAuthor = async (req, res) => {
         if (!authorRecord) {
             return res.status(404).json({error: "Author not found"});
         }
-        const publishers = await Book.aggregate('publisher', 'DISTINCT', {
-            plain: false,
-            include: {
-                model: Author,
-                as: 'authors',
-                where: {name: author},
-                through: { attributes: [] }
-            }
+        const publishers = await sequelize.query(`
+            SELECT DISTINCT b.publisher
+            FROM books b
+                     JOIN books_authors ba ON b.isbn = ba.isbn
+                     JOIN authors a ON a.name = ba.authorName
+            WHERE a.name = :name
+        `, {
+            replacements: {name: author},
+            type: sequelize.QueryTypes.SELECT
         });
-        return res.json(publishers.map(p => p.DISTINCT));
+        return res.json(publishers.map(p => p.publisher));
     } catch (e) {
         console.error('Error finding publishers by author', e);
         return res.status(500).json({
