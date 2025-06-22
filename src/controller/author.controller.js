0ea8c7c609ee437f91e51f6c84feca8382a74c6e
edward <edwardlfish@gmail.com>
2025-06-22 10:40:10 +0300

    Add remove author

--- src/controller/author.controller.js
+++ src/controller/author.controller.js
@@ -1,3 +1,4 @@
+import {sequelize} from "../config/database.js";
 import {Author, Book} from "../model/index.js";
 
 export const findBookAuthors = async (req, res) => {
@@ -20,3 +21,41 @@ export const findBookAuthors = async (req, res) => {
         })
     }
 }
+
+export const removeAuthor = async (req, res) => {
+    const t = await sequelize.transaction();
+
+    try {
+        const { author } = req.params;
+        const authorRecord = await Author.findByPk(author, { transaction: t });
+
+        if (!authorRecord) {
+            await t.rollback();
+            return res.status(404).json({ error: 'Author not found' });
+        }
+
+        const authorBooks = await authorRecord.getBooks({ transaction: t });
+
+        if (authorBooks.length > 0) {
+            await t.rollback();
+            return res.status(400).json({
+                error: 'Cannot delete author with existing books',
+                message: 'This author has associated books.'
+            });
+        }
+
+        const response = {
+            name: authorRecord.name,
+            birthDate: authorRecord.birthDate
+        };
+
+        await authorRecord.destroy({ transaction: t });
+
+        await t.commit();
+        return res.json(response);
+    } catch (error) {
+        await t.rollback();
+        console.error('Error removing author:', error);
+        return res.status(500).json({ error: 'Failed to remove author' });
+    }
+};
